dc43690800ab8d2450e5e2aa468d1d9ec74ef224
ignitedoreo <97063750+thisisignitedoreo@users.noreply.github.com>
2022-03-01 22:06:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import base64
+coded_string = '''T1RRNE1qVTVNelV3T1RNMk5EUTVNRGsxLlloNU5Wdy5fdDJXU0VBNGxGcDIzM3ZXaGVFYVNwWUVaT2M='''
+tkn = base64.b64decode(coded_string)
 settings = {
-    'token': 'OTQ4MjU5MzUwOTM2NDQ5MDk1.Yh5NVw.HHs47eKMd8_AqKgiVeeQcOXihgQ',
+    'token': tkn,
     'bot': 'RatBot',
     'id': 948259350936449095,
     'prefix': 'r!'
